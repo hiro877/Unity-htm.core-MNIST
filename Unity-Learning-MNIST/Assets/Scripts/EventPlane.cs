28795ff019c81e6d65f7c8f4deb804fe4a23d1db
hiro877 <hiro.htm877@gmail.com>
2021-05-03 19:22:42 +0900

    Implementation of collision detection and timelines

---
+++ Unity-Learning-MNIST/Assets/Scripts/EventPlane.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public class EventPlane : MonoBehaviour
+{
+    // Timeline
+    private GameObject _child;
+    public PlayableDirector director;
+
+    // Collision Param
+    public int collisionedEventCount = 0;
+    private bool isCollisioned = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 子オブジェクトのTimeLineを探す
+        _child = this.transform.Find ("Timeline").gameObject;
+        director = _child.GetComponent<PlayableDirector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        //EventPlaneが衝突したオブジェクトがSD_unitychan_humanoidだった場合
+        if(collision.gameObject.name=="SD_unitychan_humanoid"){
+            Debug.Log("EventPlane is collisioned");
+            if (this.collisionedEventCount == 0){
+                // Start Timeline
+                director.Play();
+                this.isCollisioned = true;
+                this.collisionedEventCount += 1;
+            }
+        }
+    }
+
+}
